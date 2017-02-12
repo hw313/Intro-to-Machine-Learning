6ed3299a7994b2dcc1d9c790c4c25a124d2469ed
shukoushuu <github_mail@protonmail.com>
2017-02-12 18:16:28 +0800

    Add the solutions to outliers

--- outliers/outlier_cleaner.py
+++ outliers/outlier_cleaner.py
@@ -14,7 +14,14 @@ def outlierCleaner(predictions, ages, net_worths):
     cleaned_data = []
 
     ### your code goes here
+    errors = []
+    for index in range(0, len(predictions)):
+    	errors.append(predictions[index] - net_worths[index]) 
+    largest_ten_percent_indices = sorted(range(len(errors)), key=lambda i: errors[i])[-len(predictions)/10:]
 
+    for index in range(0, len(predictions)):
+    	if index not in largest_ten_percent_indices:
+    		cleaned_data.append((ages[index], net_worths[index], errors[index]))
     
     return cleaned_data
 
--- outliers/outlier_removal_regression.py
+++ outliers/outlier_removal_regression.py
@@ -25,13 +25,21 @@ ages_train, ages_test, net_worths_train, net_worths_test = train_test_split(ages
 
 ### fill in a regression here!  Name the regression object reg so that
 ### the plotting code below works, and you can see what your regression looks like
+from sklearn.linear_model import LinearRegression
+reg = LinearRegression()
+reg.fit(ages_train, net_worths_train)
 
+slope = reg.coef_
+print "Slope: ", slope
 
+intercept = reg.intercept_
+print "Intercept: ", intercept
 
+training_score = reg.score(ages_train, net_worths_train)
+print "Training score: ", training_score
 
-
-
-
+test_score = reg.score(ages_test, net_worths_test) 
+print "Test score: ", test_score
 
 
 
@@ -68,6 +76,18 @@ if len(cleaned_data) > 0:
     ### refit your cleaned data!
     try:
         reg.fit(ages, net_worths)
+        slope = reg.coef_
+        print "Slope: ", slope
+
+        intercept = reg.intercept_
+        print "Intercept: ", intercept
+
+        training_score = reg.score(ages_train, net_worths_train)
+        print "Training score: ", training_score
+
+        test_score = reg.score(ages_test, net_worths_test) 
+        print "Test score: ", test_score
+        
         plt.plot(ages, reg.predict(ages), color="blue")
     except NameError:
         print "you don't seem to have regression imported/created,"
